1d4ffe6d26e2e800f479ce5973c9e468d2fabdff
Lucas Leandro <163912927+Lucas5812@users.noreply.github.com>
2026-05-07 09:54:47 -0300

    Add files via upload

---
+++ Garagem.js
@@ -0,0 +1,7 @@
+let CDE = {vulgo:"Cara dos emails", Nome:"Lucas", Sobrenome:"Leandro", Curso:"Tecnólogo em análise e desenvolvimento de sistemas", Aproveitamento:100};
+
+const mostradata = new Date;
+document.getElementById("cabecalho").innerHTML = mostradata;
+
+
+console.log(CDE);
---
+++ backend.js
@@ -0,0 +1,55 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", window.alert("Não acredite nas mentiras dele, exploda tudo"));
+
+let brexiste = false;
+
+const resolver = document.createElement("button");
+
+resolver.textContent = "DESFAZER";
+resolver.onclick = function resolva() {
+document.body.style.background= "rgb(57, 54, 54)";
+document.getElementById('formulario').style.display = 'block';
+
+document.getElementById('texto').textContent="não posso deixar você prosseguir sem antes de obter algumas informações, então o que me diz de responder a esse pequeno formulário para que possamos melhor te atender?";
+
+document.getElementById('titulo').textContent="Calma lá!";
+resolver.remove();
+adentrar.remove();
+document.getElementById('botao').style.display = 'inline-block';
+
+        
+};
+
+const adentrar = document.createElement("button");
+adentrar.textContent = "(por favor não) A D E N T R E  A  F E N D A";
+adentrar.onclick = window.location.href("Garagem.html")
+
+function exploda(){
+document.getElementById('formulario').style.display = 'none';
+document.getElementById('texto').textContent="DESFAÇA, RESOLVA, NÃO IMPORTA APENAS CONSERTE NÃO POSSO DEIXAR ELE SE LIBERTAR NÃO, Ņ̷̡̬̣̮̭͍̝͖͇͚͕̙̙̠̟̝͓̲̫̟̱̼̱̭̖̘͔̹͉̱̺̗͔̠̠̯͉͓̻̤̮̺͓̘̤̻͕̟̫̱̞̩̗̣̪̗̦͕̲̯͕͔̩̪̻̲̼̜̪͖̙̯̱͔̬̥̘̫̫̯̞̟̟̎̽̆́̆͂̓̍̀̋̔̉̀͂̍͑̚̚ͅͅͅͅĄ̶̨̢̧̛̛͚̪̭͓̥̥̜̯͓̯̮̩̻̳̙̦̹̃̔̊̏͋͒̂̈́̃͑̋̿̎͌̽̾͐̐̅̂̀́̏̊́͛͑̉̐̐̃̒̍̈̑̓̆̈́̈́͑͗͌͑̾̆̍̆͂́̒̈́̚͝͝͝ͅͅO̴̡̨͔̬͈͚̰̬̯̬̼͓̹͉͉̘̳̗̥͍͂̾̏͒̌͊̑̾͗́̾̉͋͂͂͂̋̉͛͊̕͠͝͝";
+document.getElementById('titulo').textContent="O QUE VOCÊ FEZ???";
+document.getElementById('titulo').style.color= "white";
+document.getElementById('titulo').style.fontSize= "25px";
+
+document.body.style.background = "url('https://thumbs.dreamstime.com/b/futuristic-glitch-background-abstract-pixel-noise-glitch-error-video-damage-like-vhs-glitch-pattern-wallpaper-design-screen-152005259.jpg')";
+
+if (!brexiste){
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+};
+
+document.body.appendChild(resolver);
+
+if (!brexiste){
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+};
+document.body.appendChild(adentrar);
+brexiste = true;
+document.getElementById('botao').style.display = 'none'
+
+}
+
